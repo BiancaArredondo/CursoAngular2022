9be58f88bf9a27a744f6deeffccea17607be24e3
Bianka <bianca@gmail.com>
2023-07-26 08:57:08 -0700

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Tarjeta{
+  titulo:string;
+  subtitulo:string;
+  nro?:number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'MiprimerApp';
+
+  public ArregloTarjetas: Tarjeta[]=[]
+
+  ngOnInit(): void {
+
+    this.ArregloTarjetas = [
+
+      {titulo:'video 1', subtitulo: 'subtitulo video 1'},
+      {titulo:'video 2', subtitulo: 'subtitulo video 3'},
+      {titulo:'video 3', subtitulo: 'subtitulo video 3'},
+    ]
+  }
 }
